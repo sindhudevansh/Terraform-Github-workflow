72e067fa1ded6ad1cc3820c33549719c6b18ace9
sindhudevansh <sindhudevansh123@gmail.com>
2023-09-27 21:02:42 +0530

    updated files

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,77 +1,68 @@
-resource "azurerm_resource_group" "rg" {
-  name     = "${var.resource_group}"
-  location = "${var.location}"
+# Configure the Azure provider
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0.2"
+    }
+  }
 }
 
-resource "azurerm_network_security_group" "nsg" {
-  name                = "${var.network_security_group}"
-  location            = "${azurerm_resource_group.rg.location}"
-  resource_group_name = "${azurerm_resource_group.rg.name}"
+provider "azurerm" {
+  features {}
 }
 
-resource "azurerm_virtual_network" "vnet" {
-  name                = "${var.vnet}"
-  location            = "${azurerm_resource_group.rg.location}"
-  resource_group_name = "${azurerm_resource_group.rg.name}"
-  address_space       = ["${var.address_space}"]
+resource "azurerm_resource_group" "TestRG" {
+  name     = "githubRG"
+  location = "East US"
 }
 
-resource "azurerm_subnet" "subnet" {
-  name                 = "${var.subnet_name}"
-  resource_group_name  = "${azurerm_resource_group.rg.name}"
-  virtual_network_name = "${azurerm_virtual_network.vnet.name}"
-  address_prefixes     = [var.subnet_prefix]
+resource "azurerm_virtual_network" "TestVnet" {
+  name                = "github-network"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.TestRG.location
+  resource_group_name = azurerm_resource_group.TestRG.name
 }
 
-resource "azurerm_public_ip" publicip {
-  name                = "${var.publicip}"
-  location            = "${azurerm_resource_group.rg.location}"
-  resource_group_name = "${azurerm_resource_group.rg.name}"
-  allocation_method   = "Dynamic"
+resource "azurerm_subnet" "Testsubnet" {
+  name                 = "Githubsubnet"
+  resource_group_name  = azurerm_resource_group.TestRG.name
+  virtual_network_name = azurerm_virtual_network.TestVnet.name
+  address_prefixes     = ["10.0.2.0/24"]
 }
 
-resource "azurerm_network_interface" nic {
-  name                = "${var.network-interface}"
-  location            = "${azurerm_resource_group.rg.location}"
-  resource_group_name = "${azurerm_resource_group.rg.name}"
+resource "azurerm_network_interface" "TestNIC" {
+  name                = "example-nic"
+  location            = azurerm_resource_group.TestRG.location
+  resource_group_name = azurerm_resource_group.TestRG.name
 
   ip_configuration {
-    name                          = "${var.ipname}"
-    subnet_id                     = "${azurerm_subnet.subnet.id}"
-    private_ip_address_allocation  = "Dynamic"
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.Testsubnet.id
+    private_ip_address_allocation = "Dynamic"
   }
 }
 
-resource "azurerm_virtual_machine" vm {
-  name                = "${var.virtual-machine}"
-  resource_group_name = "${azurerm_resource_group.rg.name}"
-  location            = "${azurerm_resource_group.rg.location}"
-  vm_size             = "${var.vm-size}"
+resource "azurerm_windows_virtual_machine" "TestVM" {
+  name                = "githubVM"
+  resource_group_name = azurerm_resource_group.TestRG.name
+  location            = azurerm_resource_group.TestRG.location
+  size                = "Standard_F2"
+  admin_username      = "adminuser"
+  admin_password      = "P@$$w0rd1234!"
   network_interface_ids = [
-    azurerm_network_interface.nic.id,
+    azurerm_network_interface.TestNIC.id,
   ]
 
-  storage_image_reference {
-    publisher = "${var.source_image_publisher}"
-    offer     = "${var.source_image_offer}"
-    sku       = "${var.source_image_sku}"
-    version   = "${var.source_image_version}"
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
   }
 
-  storage_os_disk {
-    name              = "${var.osdisk}"
-    create_option     = "FromImage"
-    caching           = "ReadWrite"
-    managed_disk_type = "Standard_LRS"
-  }
-
-  os_profile {
-    computer_name  = "${var.computername}"
-    admin_username = "${var.username}"
-    admin_password = "${var.password}"
-  }
-
-  os_profile_linux_config {
-    disable_password_authentication = false
+  source_image_reference {
+    publisher = "MicrosoftWindowsServer"
+    offer     = "WindowsServer"
+    sku       = "2016-Datacenter"
+    version   = "latest"
   }
 }
